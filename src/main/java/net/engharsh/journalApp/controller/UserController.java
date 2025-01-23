2deff09ec86b866c7cce76b87e8b5a98f2dc10ba
Harsh Patil <itsharshhh6@gmail.com>
2025-01-23 20:34:26 +0530

    git commit

--- src/main/java/net/engharsh/journalApp/controller/UserController.java
+++ src/main/java/net/engharsh/journalApp/controller/UserController.java
@@ -2,12 +2,15 @@ package net.engharsh.journalApp.controller;
 
 import net.engharsh.journalApp.entity.JournalEntry;
 import net.engharsh.journalApp.entity.User;
+import net.engharsh.journalApp.repository.UserRepository;
 import net.engharsh.journalApp.service.JournalEntryService;
 import net.engharsh.journalApp.service.UserService;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
@@ -17,33 +20,31 @@ import java.util.Optional;
 @RestController
 @RequestMapping("/user")
 public class UserController {
+
+
     @Autowired
     private UserService userService;
 
-    @GetMapping
-    public List<User> getAllUser(){
-        return userService.getAll();
-    }
 
-    @PostMapping
-    public ResponseEntity<?> createUser(@RequestBody User user){
-        try{
-        userService.saveEntry(user);
-        return new ResponseEntity<>(HttpStatus.CREATED);
-        } catch (Exception e) {
-            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-        }
-    }
+    @Autowired
+    private UserRepository userRepository;
 
-    @PutMapping("/{userName}")
-    public ResponseEntity<?> updateUser(@RequestBody User user, @PathVariable String userName){
+    @PutMapping
+    public ResponseEntity<?> updateUser(@RequestBody User user){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String userName = authentication.getName();
         User userInDb = userService.findByUserName(userName);
-        if(userInDb != null){
             userInDb.setUserName(user.getUserName());
             userInDb.setPassword(user.getPassword());
             userService.saveEntry(userInDb);
-            return new ResponseEntity<>(userInDb, HttpStatus.OK);
-        }
-        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @DeleteMapping
+    public ResponseEntity<?> deleteUserById(){
+        Authentication authentication =  SecurityContextHolder.getContext().getAuthentication();
+        userRepository.deleteByUserName(authentication.getName());
+       return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
