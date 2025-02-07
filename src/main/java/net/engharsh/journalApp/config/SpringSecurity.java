fe8898875fc37ea00c9920433a895ab71bddbc51
Harsh Patil <itsharshhh6@gmail.com>
2025-02-07 20:07:44 +0530

    git add . git commit

--- src/main/java/net/engharsh/journalApp/config/SpringSecurity.java
+++ src/main/java/net/engharsh/journalApp/config/SpringSecurity.java
@@ -1,12 +1,10 @@
 package net.engharsh.journalApp.config;
-
 import net.engharsh.journalApp.service.UserDetailsServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
@@ -15,6 +13,8 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+
+
 
 @Configuration
 @EnableWebSecurity
