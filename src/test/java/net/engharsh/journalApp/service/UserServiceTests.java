58a1f992840bb26bedde115da52cdf530c65f313
Harsh Patil <itsharshhh6@gmail.com>
2025-02-21 21:07:50 +0530

    git commit

--- src/test/java/net/engharsh/journalApp/service/UserServiceTests.java
+++ src/test/java/net/engharsh/journalApp/service/UserServiceTests.java
@@ -1,25 +1,25 @@
-package net.engharsh.journalApp.service;
-
-import net.engharsh.journalApp.repository.UserRepository;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-
-@SpringBootTest
-public class UserServiceTests {
-    @Autowired
-    private UserRepository userRepository;
-
-    @ParameterizedTest
-    @CsvSource({
-            "1,1,2"
-    })
-    public void test(int a, int b, int e){
-        assertEquals(e, a+b);
-    }
-}
+//package net.engharsh.journalApp.service;
+//
+//import net.engharsh.journalApp.repository.UserRepository;
+//import org.junit.jupiter.api.Test;
+//import org.junit.jupiter.params.ParameterizedTest;
+//import org.junit.jupiter.params.provider.CsvSource;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//import static org.junit.jupiter.api.Assertions.assertEquals;
+//import static org.junit.jupiter.api.Assertions.assertNotNull;
+//
+//@SpringBootTest
+//public class UserServiceTests {
+//    @Autowired
+//    private UserRepository userRepository;
+//
+//    @ParameterizedTest
+//    @CsvSource({
+//            "1,1,2"
+//    })
+//    public void test(int a, int b, int e){
+//        assertEquals(e, a+b);
+//    }
+//}
