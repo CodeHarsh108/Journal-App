8a7f3f27057b7aac41c39c86a25784e49f942a92
Harsh Patil <itsharshhh6@gmail.com>
2025-02-27 20:39:37 +0530

    -git commit

--- src/main/java/net/engharsh/journalApp/service/WeatherService.java
+++ src/main/java/net/engharsh/journalApp/service/WeatherService.java
@@ -2,6 +2,7 @@ package net.engharsh.journalApp.service;
 
 import net.engharsh.journalApp.api.response.WeatherResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -25,7 +26,12 @@ public class WeatherService {
     public WeatherResponse getWeather(String city){
 //        String finalAPI =  API.replace("CITY", city).replace( "API_KEY", apiKey);
         String finalAPI = String.format(API, apiKey, city);
-        ResponseEntity<WeatherResponse> response = restTemplate.exchange(finalAPI, HttpMethod.POST, null, WeatherResponse.class);
+        String requestBody = "{\n" +
+                "    \"userName\":\"MP\",\n" +
+                "    \"password\":\"Indore\"\n" +
+                "}";
+        HttpEntity<String> httpEntity = new HttpEntity<>(requestBody);
+        ResponseEntity<WeatherResponse> response = restTemplate.exchange(finalAPI, HttpMethod.POST, httpEntity, WeatherResponse.class);
         WeatherResponse body = response.getBody();
         return body;
     }
