a6e7bd923cd30049a48f6e90892bbf628338cdb9
Harsh Patil <itsharshhh6@gmail.com>
2025-03-01 21:19:56 +0530

    -git commit -m"Use of Value Annotation"

--- src/main/java/net/engharsh/journalApp/service/WeatherService.java
+++ src/main/java/net/engharsh/journalApp/service/WeatherService.java
@@ -2,6 +2,7 @@ package net.engharsh.journalApp.service;
 
 import net.engharsh.journalApp.api.response.WeatherResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
@@ -11,7 +12,10 @@ import org.springframework.web.client.RestTemplate;
 
 @Service
 public class WeatherService {
-    private static  final String apiKey = "0d8171171c8c2eb110f4c7c2fab87116";
+    @Value("${weather.api.key}")
+    private String apiKey;
+
+
     private static final String API = "http://api.weatherstack.com/current?access_key=API_KEY&query=CITY";
 //
 //    @Autowired
