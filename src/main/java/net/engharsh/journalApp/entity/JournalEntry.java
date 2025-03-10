61ad4dab4d79a38b545e295e038c720317d62ddf
Harsh Patil <itsharshhh6@gmail.com>
2025-03-10 14:19:51 +0530

    -git commit

--- src/main/java/net/engharsh/journalApp/entity/JournalEntry.java
+++ src/main/java/net/engharsh/journalApp/entity/JournalEntry.java
@@ -3,6 +3,7 @@ package net.engharsh.journalApp.entity;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.NonNull;
+import net.engharsh.journalApp.enums.Sentiment;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
